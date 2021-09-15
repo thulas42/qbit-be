57380154f39130fbd54f503fe9482c77e15013f4
Thulani Zondo <tzondo@DVTL101597.local>
2021-09-15 08:54:41 +0200

    add : update the gitignore lost

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 const axios = require('axios');
+const { apiKey} = require('./config');
 
 app.get('/currencies', (req, res, next) => {
-    
-    try {
-        res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-      axios.get(`http://data.fixer.io/api/latest?access_key=0bdc6b2ee9ebb3811b9c02a31313ae31&format=1`)
-        .then(response =>{
-            res.status(200).send( response.data)
+
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    axios.get(`http://data.fixer.io/api/latest?access_key=${apiKey}`,
+        { responseType: 'json' })
+        .then(response => {
+            res.status(200).send(JSON.stringify(response.data))
         })
-        return;
-    } catch (error) {
-        res.status(400).send(error.message || error);
-    }
-})
+        .catch((error) => {
+            res.status(400).send(error.message || error);
+        })
+    })
 
 app.listen(port, () => {
     console.log(`app listening at port ${port}`)
